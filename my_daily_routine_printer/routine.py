0f360237696a95d309767265a7526c2d48b5608a
vedshah0207-bit <vedshah0207@gmail.com>
2026-09-24 17:48:37 +0400

    my daily routine homework

---
+++ my_daily_routine_printer/routine.py
@@ -0,0 +1,7 @@
+print("My Daily Routine")
+
+print("I wake up at 6 AM.")
+print("I go to school at 7 AM.")
+print("I play with my friends in the evening.")
+print("I study for 2 hours at night.")
+print("Total study hours in a week:", 2 * 7)
